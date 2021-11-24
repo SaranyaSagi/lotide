09a86ac152f93ced1274ae5e1b882966b40826e0
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-24 14:32:49 -0500

    function that takes in string and returns object with counts of each letter in string

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(` 🛑 Assertion Failed: ${actual} !== ${expected}`);
+    //console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== '  + expected);
+  } else {
+    console.log(` ✅ Assertion Passed: ${actual} === ${expected}`);
+    //console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  }
+};
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+const countLetters = function (String) {
+  const counts = {}
+
+  for (const letter of String) {
+    //console.log(letter);
+    if (counts[letter]) {
+      counts[letter] += 1; 
+    } else {
+      counts[letter] = 1
+    }
+  }
+  return counts;
+}
+
+console.log(countLetters("hellooutthere"));
+const result2 = countLetters("hellooutthere");
+assertEqual(result2["h"], 2);
+assertEqual(result2["w"], undefined);
+assertEqual(result2["r"], 1);
+
+//assertEqual(result2[""], 2);
