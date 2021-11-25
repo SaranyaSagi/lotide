9eb26cd2a8a18870966cd9af31f638b99f04349c
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-24 19:34:42 -0500

    update to older function now checks array lengths first before running thorugh array

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,8 @@
 const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  
   for (let i=0; i< arrayA.length; i++) {
     if (arrayA[i] !== arrayB[i]) {
       return false;
