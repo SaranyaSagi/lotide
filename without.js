44aa6c2b1dd3531018bc94855c8f64f17949e8e2
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-24 23:55:26 -0500

    fixed lint errors with eslint

--- without.js
+++ without.js
@@ -1,33 +1,33 @@
 //Implement 'without' which will return a subset of given array, removing unwanted elements
 //function(sourceArray, itemsToRemoveArray)
-//return new array which has itemsToRemove items removed from source array. 
+//return new array which has itemsToRemove items removed from source array.
 //without([1, 2, 3,], [1]) --> [2,3]
-//new array should not modify original array. maybe use slice. 
-//so test for that in test cases. 
+//new array should not modify original array, maybe use slice.
+//so test for that in test cases.
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
-  console.log(eqArrays(arrayOne, arrayTwo) === true? "✅" : "🛑");
+  console.log(eqArrays(arrayOne, arrayTwo) === true ? "✅" : "🛑");
 };
 
 const eqArrays = function(arrayA, arrayB) {
-  for (let i=0; i< arrayA.length; i++) {
+  for (let i = 0; i < arrayA.length; i++) {
     if (arrayA[i] !== arrayB[i]) {
       return false;
     }
   }
   return true;
-}
+};
 //console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 
-const without = function (source, itemsToRemove) {
+const without = function(source, itemsToRemove) {
   let newArray = [];
-  for (let i = 0; i < source.length; i++) {   //if I try for (let item of source) { --> final newArray --> [undefined, undef, undef]
-    if (!itemsToRemove.includes(source[i])) {   //(.includes is key) here it means if itemstoRemove does not include because of ! 
-      newArray.push(source[i])
+  for (let i = 0; i < source.length; i++) {  //if I try for (let item of source) { --> final newArray --> [undefined, undef, undef]
+    if (!itemsToRemove.includes(source[i])) {   //(.includes is key) here it means if itemstoRemove does not include because of !
+      newArray.push(source[i]);
     }
   }
   return newArray;
-}
+};
 
 const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
