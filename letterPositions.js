e866e17488a1a42287dc718b4972309bcb91608a
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-24 22:27:27 -0500

    updated after fixing lint errors using eslint

--- letterPositions.js
+++ letterPositions.js
@@ -1,26 +1,26 @@
- //for each value of sentence
-  //find index anywhere it occurs
-  //push to [] for that item value
+//for each value of sentence
+//find index anywhere it occurs
+//push it to [] for that item value
 
- //create array for each character
-    //then push index values into it
-    //then add these arrays as key-values to results 
+//create array for each character
+//then push index values into it
+//then add these arrays as key-values to results
 
 const letterPositions = function(sentence) {
   const results = {};
-  for (let i=0; i < sentence.length; i++) {
-    if(!results[sentence[i]]) {            //h is not in results
-      results[sentence[i]] = [i]        //then the index goes into the array
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) {            //h is not in results
+      results[sentence[i]] = [i];        //then the index goes into the array
     } else {
       //console.log(sentence[i])          //for repeating ones it already exists
-      results[sentence[i]].push(i);    // so pushing that new index in as well. 
-    }   
+      results[sentence[i]].push(i); // so pushing that new index in as well.
+    }
   }
   return results;
-}
+};
 
 const eqArrays = function(arrayA, arrayB) {
-  for (let i=0; i< arrayA.length; i++) {
+  for (let i = 0; i < arrayA.length; i++) {
     if (arrayA[i] !== arrayB[i]) {
       return false;
     }
@@ -29,12 +29,12 @@ const eqArrays = function(arrayA, arrayB) {
 };
 
 const assertArraysEqual = function(arrayA, arrayB) {
-  console.log(eqArrays(arrayA, arrayB) === true? "✅" : "🛑");
+  console.log(eqArrays(arrayA, arrayB) === true ? "✅" : "🛑");
 };
 //console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 //assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 
-let result3 = letterPositions("hello")
+let result3 = letterPositions("hello");
 console.log(result3);
 assertArraysEqual(letterPositions("hello").e, [1]);
 
