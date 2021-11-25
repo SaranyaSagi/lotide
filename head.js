ee4de53d56fd02f303fce1e0c60ce2b369904694
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-24 23:57:34 -0500

    fixed lint errors with eslint

--- head.js
+++ head.js
@@ -10,13 +10,13 @@ const assertEqual = function(actual, expected) {
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1);
 
-const head = function (array) {
+const head = function(array) {
   let first = array[0];
   return first;
-}
+};
 
-assertEqual(head([5, ,6, 7]), 5); 
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5, 6, 7]), 8); 
-assertEqual(head([1]), 1); 
-assertEqual(head([]), undefined); 
+assertEqual(head([5, 6, 7]), 8);
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
