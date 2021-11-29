0205fe0fcad922c3e34f4879ce99e65f581d8a0d
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-29 16:57:43 -0500

    incorporated mocha and chai tests using describe and it method and used assert.deepEqual instead of strictEqual because arrays are involved

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,21 @@
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
+//const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle');
 
-// console.log(middle([1, 2, 3, 4, 5, 6]));
-// console.log(middle([1, 2, 3, 4, 5]));
-// console.log(middle([8]));
-// console.log(middle([8, 2]));
+describe("#middle", () => {
+  it("returns the 2 middle numbers for even lists [1, 2, 3, 4, 5, 5, 6,]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => should PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => should PASS
-assertArraysEqual(middle([8]), []); // => should PASS
-assertArraysEqual(middle([8, 2]), []); // => should PASS
+  it("returns 1 middle number [3] for odd lists [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
+  });
+  
+  it("returns empty array [] for array with single number ", () => {
+    assert.deepEqual(middle([8]), []); 
+  });
+
+  it("returns empty array [] for array with two numbers", () => {
+    assert.deepEqual(middle([8, 2]), []); 
+  });
+});
