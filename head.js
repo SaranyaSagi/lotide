d40cd6922598aadc6d2e1788ea6bedabf20bebc6
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-29 15:35:53 -0500

    exported function using module.exports, moved test cases to separate test file that requires export using a path and now files are cleaner

--- head.js
+++ head.js
@@ -1,22 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(` 🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(` ✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-//test code
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   let first = array[0];
   return first;
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5, 6, 7]), 8);
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+module.exports = head;
