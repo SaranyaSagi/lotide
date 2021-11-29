fec173e979f4ad25e25c917149402604e9d11524
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-29 15:56:28 -0500

    refactored to use modules and separated test cases to new file, access function using require after exporting using module.exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,9 @@
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arrayA, arrayB) {
   console.log(eqArrays(arrayA, arrayB) === true ? "✅" : "🛑");
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+module.exports = assertArraysEqual;
+
 
